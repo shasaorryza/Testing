49c68ffd77f5ebcdb7668660cbf403d40f1a86c4
shasaorryza <shasaorryza13@gmail.com>
2015-08-31 23:08:55 +0700

    Create exercise3.cpp
    Nama     : Orryza Dwi Juni Ayuningtyas
    NIM       : 1301144416
    Kelas     : IF 38-06

---
+++ exercise3.cpp
@@ -0,0 +1,14 @@
+Kamus :
+x,y,z : integer;
+
+Algorithm :
+input(x);
+input(y);
+input(z);
+
+if (x=y) and (y=z) then
+ output("Segitiga sama sisi")
+else if (x=y) and (y<>z) then
+ output("Segitiga sama kaki")
+else
+ output("Segitiga sembarang")
